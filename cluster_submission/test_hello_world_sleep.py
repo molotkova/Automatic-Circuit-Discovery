d7ec03be7bb28a51ae966f5fe20a203336fab1ca
molotkova_s <molotkova548@gmail.com>
2025-08-23 18:47:53 +0200

    fixed .out and .err files generation during job run

---
+++ cluster_submission/test_hello_world_sleep.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import time
+import sys
+
+print("hello world")
+print("Sleeping for 10 seconds...")
+sys.stderr.write("This is a test message to stderr\n")
+time.sleep(10)
+print("hello world")
